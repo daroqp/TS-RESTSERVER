51a7aa67b8fb6bd14be94b70bcf834b66c3faea5
Dario Quispe <daro.qp@gmail.com>
2022-06-30 23:29:29 -0300

    feat: add middlewares

--- src/main/routes/auth.route.ts
+++ src/main/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
 
 export default (router: Router): void => {
-    router.get("/auth", (req, res) => res.send("hola"));
+    router.get("/auth", (req, res) => res.send("hola { controller }"));
 };
